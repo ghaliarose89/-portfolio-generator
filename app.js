33705ee6c514a5a2a7e1b8f574116b5c35a7380e
unknown <ghaliarose89@gmail.com>
2021-10-11 14:23:06 -0500

    inquirer

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
-const fs = require ('fs');
-const generatePage = require('./src/page-template');
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-const [name , github ] = profileDataArgs;
+const inquirer  = require('inquirer');
+// const fs = require ('fs');
+// const generatePage = require('./src/page-template');
+// //const profileDataArgs = process.argv.slice(2, process.argv.length);
+// const [name , github ] = profileDataArgs;
+
+// const pageHTML = generatePage(name, github);
+
+// fs.writeFile('index.html',generatePage (name,github), err => {
+//     if (err) throw err;
+//     console.log("Portfolio complete! Check out index.html to see the output!");
+// });
 
 
+inquirer
+. prompt([
+    {
+        type:'input',
+        name:'name',
+        message:"enter your name"
+    }
 
-fs.writeFile('index.html',generatePage (name,github), err => {
-    if (err) throw err;
-    console.log("Portfolio complete! Check out index.html to see the output!");
-});
+])
+.then (answers => console.log(answers));
